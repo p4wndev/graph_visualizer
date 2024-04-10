8aec92842f10684eb7d428a1469a5c6f491f7480
tpd0701 <164672451+tpd0701@users.noreply.github.com>
2024-04-11 01:06:36 +0700

    Update search.py

--- algo_lib/search.py
+++ algo_lib/search.py
@@ -1,28 +1,82 @@
-import streamlit as st
-
-
 def bfs(graph, start_node):
-    visited = set()
-    queue = [start_node]
-    list_nodes = []
-    st.markdown("<p>Thứ tự duyệt theo chiều rộng: </p>",
-                unsafe_allow_html=True)
-    while queue:
-        node = queue.pop(0)
-        if node in visited:
-            continue
-        visited.add(node)
-        list_nodes.append(node)
-        neighbors = list(graph.neighbors(node))
-        neighbors.sort()
-        for neighbor in neighbors:
-            if neighbor not in visited:
-                queue.append(neighbor)
-    st.subheader(' → '.join(list_nodes))
-    st.divider()
-    return visited
-    # drawGraph(graph)
+    components = []
+    def BFS(graph, start_node):
+        visited = set()
+        queue = [start_node]
+        order = []
+        
+        while queue:
+            node = queue.pop(0)
+            if node in visited:
+                continue
+            visited.add(node)
+            order.append(node)
+            neighbors = [int(node) for node in graph.neighbors(node)]
+            neighbors.sort(reverse=False)
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    queue.append(str(neighbor))
+        components.append(order)
+        return visited
+    # duyệt toàn bộ đồ thị
+    nodes = set(graph.nodes) - BFS(graph, start_node)
+    while nodes:
+        lst = [int(node) for node in nodes]
+        lst.sort()
+        nodes = nodes - BFS(graph, str(lst[0]))
+    return components
 
+def dfs(graph, start_node):
+    components = []
+    def DFS(graph, start_node):
+        visited = set()
+        stack = [start_node]
+        order = []
+        
+        while stack:
+            node = stack.pop()
+            if node in visited:
+                continue
+            visited.add(node)
+            order.append(node)
+            neighbors = [int(node) for node in graph.neighbors(node)]
+            neighbors.sort(reverse=False)
+            for neighbor in neighbors:
+                if neighbor not in visited:
+                    stack.append(str(neighbor))
+        components.append(order)
+        return visited
+    # duyệt toàn bộ đồ thị
+    nodes = set(graph.nodes)-DFS(graph, start_node)
+    while nodes:
+        lst = list(nodes)
+        lst.sort()
+        nodes = nodes-DFS(graph, lst[0])
+    return components
+
+def dfs_recursion(graph, start_node):
+    visited = set()
+    components = []
+    def DFS(graph, start_node):
+        order = []
+        def recursion(node):
+            visited.add(node)
+            order.append(node)
+            neighbors = [int(node) for node in graph.neighbors(node)]
+            neighbors.sort(reverse=False)
+            for neighbor in neighbors:
+                if str(neighbor) not in visited:
+                    recursion(str(neighbor))
+        recursion(start_node)
+        components.append(order)
+        return visited
+     # duyệt toàn bộ đồ thị
+    nodes = set(graph.nodes)-DFS(graph, start_node)
+    while nodes:
+        lst = list(nodes)
+        lst.sort()
+        nodes = nodes-DFS(graph, lst[0])
+    return components
 
 '''
 Test case:
@@ -43,28 +97,3 @@ Test case:
 10 12
 11 12
 '''
-
-# Duyệt đồ thị theo chiều sâu
-
-
-def dfs(graph, start_node):
-    visited = set()
-    stack = [start_node]
-    list_nodes = []
-    st.markdown("<p>Thứ tự duyệt theo chiều sâu: </p>",
-                unsafe_allow_html=True)
-    while stack:
-        node = stack.pop()
-        if node in visited:
-            continue
-        visited.add(node)
-        list_nodes.append(node)
-        neighbors = list(graph.neighbors(node))
-        neighbors.sort()
-        for neighbor in neighbors:
-            if neighbor not in visited:
-                stack.append(neighbor)
-    st.subheader(' → '.join(list_nodes))
-    st.divider()
-    return visited
-    # drawGraph(graph)
